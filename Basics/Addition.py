73e54834d2d651f63b0b32af1988ee16b00e2e6b
sameer-palkar <sameer.palkar@jetsynthesys.com>
2025-01-31 14:16:30 +0530

    Intializing and addin the folder to git

---
+++ Basics/Addition.py
@@ -0,0 +1,8 @@
+a = int(input("Enter Firast Number"))
+b = int(input("Enter Second Number"))
+c = a+b
+print("Addition is : ",c)
+if(a>b):
+    print("A is greater than b")
+else:
+    print("B is greater than a")
